cae68d555a973abffd96fee45161ee450f98948b
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-06-04 12:36:37 -0500

    Set up header

---
+++ employee-directory/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>User Directory</h1>
+                {/* Add instructions */}
+            </div>
+        )
+    }
+}
