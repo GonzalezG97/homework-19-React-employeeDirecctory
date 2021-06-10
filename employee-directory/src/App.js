99f571cdb16c7a558e5f0aed0b8c269f62b4a96e
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-06-10 11:43:35 -0500

    Added table component

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './components/Header/Header.jsx';
+import Table from './components/Table/Table.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Table/>
     </div>
   );
 }
