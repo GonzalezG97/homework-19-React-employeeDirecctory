07a5912674d54c8a2feaee4056cac0237a856b70
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-06-13 18:12:18 -0500

    Table data loads with API

--- employee-directory/src/components/Table/Table.jsx
+++ employee-directory/src/components/Table/Table.jsx
@@ -9,22 +9,21 @@ function Table(props) {
           <thead>
             <tr>
               <th scope="col">Picture</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
+              <th scope="col">Name</th>
               <th scope="col">Email</th>
-              <th scope="col">Phone Number</th>
               <th scope="col">City</th>
+              <th scope="col">Phone Number</th>
             </tr>
           </thead>
           <tbody>
           {props.users[0]!== undefined && props.users[0].name!== undefined ?
-            (props.users.map(({name, email, city, phone, picture}) => {
+            (props.users.map(({name, email, location, phone, picture}) => {
                 return (<tr>
+                    <img src={picture.thumbnail}/>
                     <td>{name.first} {name.last}</td>
                     <td>{email}</td>
-                    <td>{city}</td>
-                    <td>{phone}</td>
-                    <img src={picture.thumbnail}/>
+                    <td>{location.city}</td>
+                    <td>{phone}</td>                    
                 </tr>)
                     }
                 ))
